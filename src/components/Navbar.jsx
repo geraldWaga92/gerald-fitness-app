0a7a191d0736155889c30c7c8fa4335a333b8152
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-22 14:26:41 -0700

    create logo and update navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import Logo from '../assets/images/fitness-logo.png';
 
-export default Navbar
+const Navbar = () => (
+  <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
+    <Link to="/">
+      <img src={Logo} alt="logo" style={{ width: 'auto', height: '60px', margin: '0px 10px' }} />
+    </Link>
+    <Stack
+      direction="row"
+      gap="40px"
+      fontFamily="Alegreya"
+      fontSize="24px"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Link to="/" style={{ textDecoration: 'none', color: '#CF0A0A', borderBottom: '3px solid #FF2625' }}>Home</Link>
+      <a href="#exercises" style={{ textDecoration: 'none', color: '#CF0A0A' }}>Exercises</a>
+    </Stack>
+  </Stack>
+);
+
+export default Navbar;
