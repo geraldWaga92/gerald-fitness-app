53b355472e00a8b0ce165a7762c31c426138d12e
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-23 11:31:40 -0700

    update app responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/fitness-logo.png';
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/fitness-logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: 'auto', height: '60px' }} />
+    <Stack
+      gap="40px"
+      sx={{ alignItems: "center" }}
+      flexWrap="wrap"
+      px="40px"
+      pt="24px"
+    >
+      <Link to="/">
+        <img src={Logo} alt="logo" style={{ width: "auto", height: "60px" }} />
+      </Link>
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} color="#4F4557" mt="30px" textAlign="center" pb="30px">Made with ❤️ by Gerald dev</Typography>
+    <Typography
+      variant="h5"
+      sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+      color="#4F4557"
+      mt="30px"
+      textAlign="center"
+      pb="30px"
+    >
+      Made with ❤️ by Gerald dev
+    </Typography>
   </Box>
 );
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Stack } from '@mui/material';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
 
-import Logo from '../assets/images/fitness-logo.png';
+import Logo from "../assets/images/fitness-logo.png";
 
 const Navbar = () => (
-  <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
+  <Stack
+    direction={{ xs: "column", md: "row" }}
+    justifyContent="space-around"
+    sx={{
+      gap: { sm: "50px", xs: "40px" },
+      mt: { sm: "32px", xs: "20px" },
+      justifyContent: "none",
+    }}
+    px="20px"
+  >
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: 'auto', height: '60px', margin: '0px 10px' }} />
+      <img
+        src={Logo}
+        alt="logo"
+        style={{ width: "auto", height: "70px", margin: "0px 10px" }}
+      />
     </Link>
     <Stack
       direction="row"
-      gap="40px"
+      gap="30px"
       fontFamily="Alegreya"
-      fontSize="24px"
+      fontSize="25px"
       alignItems="center"
       justifyContent="center"
+      sx={{ justifyContent: 'flex-start', fontSize: '35px'}}
+      // sx={{flexDirection: {sm: "column", md: "row"}}}
     >
-      <Link to="/" style={{ textDecoration: 'none', color: '#CF0A0A', borderBottom: '3px solid #FF2625' }}>Home</Link>
-      <a href="#exercises" style={{ textDecoration: 'none', color: '#CF0A0A' }}>Exercises</a>
+      <Link
+        to="/"
+        style={{
+          textDecoration: "none",
+          color: "#CF0A0A",
+          borderBottom: "2px solid #FF2625",
+        }}
+      >
+        Home
+      </Link>
+      <a href="#exercises" style={{ textDecoration: "none", color: "#CF0A0A" }}>
+        Exercises
+      </a>
     </Stack>
   </Stack>
 );
