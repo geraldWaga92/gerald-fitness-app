30c26ed66b3dd4acf0f57d1ebf68fa19a45c05d6
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-23 09:54:16 -0700

    add rapid api youtube search to fetchdata and update exerciseDetail to show data from api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -12,7 +12,7 @@ export const youtubeOptions = {
     method: "GET",
     headers: {
         "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-        "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
+        "X-RapidAPI-Key": process.env.RAPID_API_YOUTUBE_SEARCH_KEY
     },
 };
 
